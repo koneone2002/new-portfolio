6e8d88c1e2c9584a310db2cbe70589aa6a0dab3d
Katie <katie@imagings-MacBook-Pro.local>
2018-07-21 17:46:54 -0700

    almost done

--- assets/javascript/file.js
+++ assets/javascript/file.js
@@ -6,6 +6,57 @@ $(document).ready(function(){
    
     // Add smooth scrolling to all links
     $("#btn1").on('click', function(event) {
+     
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+       
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+    // Add smooth scrolling to all links
+    $("#btn2").on('click', function(event) {
+     
+     
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+       
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+
+    $("#btn3").on('click', function(event) {
+     
+     
   
       // Make sure this.hash has a value before overriding default behavior
       if (this.hash !== "") {
@@ -28,3 +79,5 @@ $(document).ready(function(){
       } // End if
     });
   });
+  
+  
